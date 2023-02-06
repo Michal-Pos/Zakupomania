1985073f4ef4e6acd279ca51aac80a79173483e5
Michal-Pos <michal.posiadala@gmail.com>
2023-02-06 12:04:33 +0100

    Przepisanie klas Minimalista i Maksymalista tak aby nie można było kupować w ulubionym Sklepie jesli jest on w Sieci

--- Maksymalista.cpp
+++ Maksymalista.cpp
@@ -5,16 +5,22 @@
 #include "Maksymalista.h"
 
 bool Maksymalista::kup() {
-    tuple<string, float, unsigned int> najdro = ulubiony->daj_najdrozszy();
-    string nazwa = get<0>(najdro);
-    float cena = get<1>(najdro);
-    unsigned int ilosc = get<2>(najdro);
-    if (ilosc>0 && cena< budzet){//jeśli Mieszkańca stać na dany przedmiot
-        budzet-= cena;
-        ulubiony->sprzedaj_Towar(nazwa,1);
-        cout<<"Udało się kupić najdroższy towar"<<nazwa <<" w cenie "<<cena<<endl;
-        return true;
-    };
+    if (!ulubiony->czy_w_Sieci()){
+        tuple<string, float, unsigned int> najdro = ulubiony->daj_najdrozszy();
+        string nazwa = get<0>(najdro);
+        float cena = get<1>(najdro);
+        unsigned int ilosc = get<2>(najdro);
+        if (ilosc>0 && cena< budzet){//jeśli Mieszkańca stać na dany przedmiot
+            budzet-= cena;
+            ulubiony->sprzedaj_Towar(nazwa,1);
+            cout<<"Udało się kupić najdroższy towar"<<nazwa <<" w cenie "<<cena<<endl;
+            return true;
+        };
+    }else cout<<"Sklep znajduje się w Sieci, nie mozna w nim kupic towaru"<<endl;
+    return false;
+
+
+
     return false;
 
 }
--- Minimalista.cpp
+++ Minimalista.cpp
@@ -5,16 +5,20 @@
 #include "Minimalista.h"
 
 bool Minimalista::kup() {
-    tuple<string, float, unsigned int> najtan = ulubiony->daj_najtanszy();
-    string nazwa = get<0>(najtan);
-    float cena = get<1>(najtan);
-    unsigned int ilosc = get<2>(najtan);
-    if (ilosc>0 && cena< budzet){//jeśli Mieszkańca stać na dany przedmiot
-        budzet-= cena;
-        ulubiony->sprzedaj_Towar(nazwa,1);
-        cout<<"Udało się kupić najtanszy towar"<<nazwa <<" w cenie "<<cena<<endl;
-        return true;
-    };
+    if(!ulubiony->czy_w_Sieci()){
+        tuple<string, float, unsigned int> najtan = ulubiony->daj_najtanszy();
+        string nazwa = get<0>(najtan);
+        float cena = get<1>(najtan);
+        unsigned int ilosc = get<2>(najtan);
+        if (ilosc>0 && cena< budzet){//jeśli Mieszkańca stać na dany przedmiot
+            budzet-= cena;
+            ulubiony->sprzedaj_Towar(nazwa,1);
+            cout<<"Udało się kupić najtanszy towar"<<nazwa <<" w cenie "<<cena<<endl;
+            return true;
+        };
+        cout<<"Mieszkaniec nie ma wystarczającej ilości środków"<<endl;
+    } else cout<<"Sklep znajduje się w Sieci, nie mozna w nim kupic towaru"<<endl;
+
     return false;
 
 }
