5d654cd67af7d7782e55065bbce13bff96e38870
Michal-Pos <michal.posiadala@gmail.com>
2023-02-07 19:56:06 +0100

    dodanie pliku nagłówkowego dla Poszukiwacz

--- Tradycjny.h
+++ Tradycjny.h
@@ -8,6 +8,15 @@
 
 class Tradycjny: public Poszukiwacz{
 public:
+    bool kup();
+
+    Tradycjny(float _x, float _y, double _budzet, string _szukany_T): Poszukiwacz(_budzet, _x, _y, _szukany_T){}
+
+    Tradycjny(const Tradycjny&) = default;
+    Tradycjny& operator=(const Tradycjny&) = default;// kopiowanie obiektu
+    ~Tradycjny() = default;
+    Tradycjny(Tradycjny&&) = default;
+    Tradycjny& operator=(Tradycjny&&) = default;
 };
 
 
