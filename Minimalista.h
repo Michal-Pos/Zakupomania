c05ec018c033f4048b0360fd30958d5a0e514ac1
Michal-Pos <michal.posiadala@gmail.com>
2023-02-11 14:02:10 +0100

    zmiana wszystkich metdo technicznych w Minimalista.h na delete

--- Minimalista.h
+++ Minimalista.h
@@ -7,17 +7,18 @@
 #include "Wierny.h"
 #include "Sklep.h"
 
-class Minimalista: public Wierny {public:
+class Minimalista: public Wierny {
+public:
     bool kup();
     void wypisz_stan();
     Minimalista(Sklep* Sk, float _budzet, float _x, float _y): Wierny(Sk,_budzet, _x, _y){};
 
     //techniczne
-    Minimalista() = default;
-    Minimalista(const Minimalista&) = default;
-    Minimalista& operator=(const Minimalista&) = default;
-    Minimalista(Minimalista&&) = default;
-    Minimalista& operator=(Minimalista&&) = default;
+    Minimalista() = delete;
+    Minimalista(const Minimalista&) = delete;
+    Minimalista& operator=(const Minimalista&) = delete;
+    Minimalista(Minimalista&&) = delete;
+    Minimalista& operator=(Minimalista&&) = delete;
 
 };
 
